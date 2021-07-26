cd05d58fd681b17079fb7ca06431ff85d227fd33
Natalia <nef.n@list.ru>
2021-07-26 13:52:35 +0300

    Initial commit

---
+++ app/src/main/java/keyone/keytwo/lesson2_dz_calculator/MainActivity.java
@@ -0,0 +1,14 @@
+package keyone.keytwo.lesson2_dz_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
